30613615ba29da58fd98142d9bef212c799bf203
emrecan-k <myveryownfakemsn@gmail.com>
2014-08-19 11:41:37 +0100

    Running logo/polygon.py

--- logo/polygon.py
+++ logo/polygon.py
@@ -2,7 +2,7 @@ from tealight.logo import move, turn
 
 def polygon(edges, size):
   angle = 360.0 / edges
-  for i in range(0, edges):
+  for i in range(1, edges):
     move(size)
     turn(angle)
     
