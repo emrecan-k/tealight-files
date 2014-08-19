cf6d81343a2fd388bc3748336eb1ede8642f6f2c
emrecan-k <myveryownfakemsn@gmail.com>
2014-08-19 12:08:47 +0100

    Running logo/chess.py

--- logo/chess.py
+++ logo/chess.py
@@ -1,15 +1,17 @@
 from tealight.logo import move, turn
 n=0
+size=30
 def square(side):
   for i in range(0,4):
     move(side)
     turn(90)
 while n < 7:
-  square(30)
-  move(30)
-  square(30)
+  square(size)
+  move(size)
+  square(size)
   n = n + 1 
 turn(270)
-square(30)
-move(30)
-square(30)
+square(size)
+turn(90)
+move(size)
+square(size)
