a7f9ee026b134a2e2e072c1cb1eedd8f85cd5d63
emrecan-k <myveryownfakemsn@gmail.com>
2014-08-19 15:14:47 +0100

    Running robot/cleanup.py

--- robot/cleanup.py
+++ robot/cleanup.py
@@ -5,5 +5,5 @@ from tealight.robot import (move,
                             smell, 
                             left_side, 
                             right_side)
-
-# Add your code here
+for i in range(0, 3)
+  move
