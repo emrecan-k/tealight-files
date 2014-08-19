b3baf6956dc969e105c2a258e7aed4d38e047b7e
emrecan-k <myveryownfakemsn@gmail.com>
2014-08-19 14:54:30 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -5,17 +5,25 @@ from tealight.robot import (move,
                             smell, 
                             left_side, 
                             right_side)
-n=0
-while n < 32:
-  move()
-  n = n+1
-turn(1)
-n = 0
-while n < 4:
-  move()
-  n +=1
-turn(1)
-n=0
-while n <32:
-  move()
-  n+=1
+i=0
+while i<4:
+  n=0
+  while n < 32:
+    move()
+    n = n+1
+  turn(1)
+  n = 0
+  while n < 4:
+    move()
+    n +=1
+  turn(1)
+  n=0
+  while n <32:
+    move()
+    n+=1
+  turn(-1)
+  n=0
+  while n<4:
+    move()
+    n+=1
+  i+=1
