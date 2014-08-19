31592924f8c22b4562e1da6753b7519b38730051
emrecan-k <myveryownfakemsn@gmail.com>
2014-08-19 14:59:43 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -32,3 +32,31 @@ n=0
 while n < 32:
  move()
  n = n+1
+turn(-1)
+i=0
+while i<4:
+  n=0
+  while n < 32:
+    move()
+    n = n+1
+  turn(-1)
+  n = 0
+  while n < 4:
+    move()
+    n +=1
+  turn(-1)
+  n=0
+  while n <32:
+    move()
+    n+=1
+  turn(1)
+  n=0
+  while n<4:
+    move()
+    n+=1
+  turn(1)
+  i+=1
+n=0
+while n < 32:
+ move()
+ n = n+1
