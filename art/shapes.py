a046535773e980527fef6e63596aea210f5f930b
emrecan-k <myveryownfakemsn@gmail.com>
2014-08-21 11:07:08 +0100

    Closing art/shapes.py

--- art/shapes.py
+++ art/shapes.py
@@ -5,3 +5,13 @@ color("blue")
 spot(300,200,20)
 circle(300,200,50)
 box(250,250,100,100)
+def handle_mousemove(x,y,button):
+  global lastx, lasty
+  
+  if button == "left":
+    
+    ("blue")
+    line(lastx, lasty, x, y)
+    lastx = x
+    lasty = y
+  
