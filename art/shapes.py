df607b7ce021cba55e6eb2ed2a506a8e49131e39
emrecan-k <myveryownfakemsn@gmail.com>
2014-08-21 11:35:53 +0100

    Running art/shapes.py

--- art/shapes.py
+++ art/shapes.py
@@ -5,7 +5,7 @@ color("blue")
 spot(300,200,20)
 circle(300,200,50)
 box(250,250,100,100)
-def handle_mousemove(x,y,button):
+def handle_mousedown(x,y,button):
   global lastx, lasty
   
   if button == "left" and 350 > x > 250 and 350 > y > 250:
