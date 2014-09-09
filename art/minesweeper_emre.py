8753b6d69598095b300417176108e86631eeefb9
emrecan-k <myveryownfakemsn@gmail.com>
2014-09-09 11:13:11 +0100

    Running art/minesweeper_emre.py

--- art/minesweeper_emre.py
+++ art/minesweeper_emre.py
@@ -169,7 +169,7 @@ def FloodBoard(x,y):
           FloodBoard(i,j)
       
 NumberOfBombs = 25
-HLimit = 10
+HLimit = 20
 WLimit = HLimit
 SquareSize = 500/HLimit
 StartingX = screen_width /2 - SquareSize * WLimit/2
